8a40c6b14c24f887f25b8849b56dcec8e0d48a94
Grant-Ellington <gellingtonem6@gmail.com>
2022-08-30 15:48:13 -0500

    added times and texgt area boxes in the HTML file

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+// 1 add in html for calendar that are time block
+//see line 32 and down for next steps
+
+//using moment.js find a way to add in the current date and time and add to the header
+
+// using moment.js find a way to to target time and change the background property with past, current and future markers
+// Past will be red, current will be green; future will be white
+
+// add js functionality on jQuery to add in a text field with if 'events'
+
+// The saved wvent needs to be saved using stringify 
