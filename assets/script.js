c98712178839a384336e555d8b10e28cd5050a36
Grant-Ellington <gellingtonem6@gmail.com>
2022-09-01 00:25:00 -0500

    added functionallity of all time colors

--- assets/script.js
+++ assets/script.js
@@ -24,14 +24,101 @@ setInterval( () => {
     clock.textContent = now   
 },1000)
 
-var div = document.getElementById('nineAM')
+const divNineAM = document.getElementById('nineAM')
+const divTenAM = document.getElementById('tenAM')
+const divElevenAM = document.getElementById('elevenAM')
+const divTwelvePM = document.getElementById('twelvePM')
+const divOnePM = document.getElementById('onePM')
+const divTwoPM = document.getElementById('twoPM')
+const divThreePM = document.getElementById('threePM')
+const divFourPM = document.getElementById('fourPM')
+const divFivePM = document.getElementById('fivePM')
 
+// changes color for 9:00 am
 if( moment(loadTime).isSame(nineAM)){
-    div.setAttribute("style", "background: green");
+    divNineAM.setAttribute("style", "background: green");
 } else if ( moment(loadTime).isBefore(nineAM)){
-    div.setAttribute("style", "background: red")
+    divNineAM.setAttribute("style", "background: red")
 }else {
-    div.setAttribute("style", "background: yellow")
+    divNineAM.setAttribute("style", "background: yellow")
+}
+// changes color for 1000
+if( moment(loadTime).isSame(tenAM)){
+    divTenAM.setAttribute("style", "background: green");
+} else if ( moment(loadTime).isBefore(tenAM)){
+    divTenAM.setAttribute("style", "background: red")
+}else {
+    divTenAM.setAttribute("style", "background: yellow")
+}
+
+//changes color for 1100
+
+if( moment(loadTime).isSame(elevenAM)){
+    divElevenAM.setAttribute("style", "background: green");
+} else if ( moment(loadTime).isBefore(elevenAM)){
+    divElevenAM.setAttribute("style", "background: red")
+}else {
+    divElevenAM.setAttribute("style", "background: yellow")
+}
+
+//changes color for 1200
+
+if( moment(loadTime).isSame(twelvePM)){
+    divTwelvePM.setAttribute("style", "background: green");
+} else if ( moment(loadTime).isBefore(nineAM)){
+    divTwelvePM.setAttribute("style", "background: red")
+}else {
+    divTwelvePM.setAttribute("style", "background: yellow")
+}
+
+//change color for 1300
+
+if( moment(loadTime).isSame(onePM)){
+    divOnePM.setAttribute("style", "background: green");
+} else if ( moment(loadTime).isBefore(onePM)){
+    divOnePM.setAttribute("style", "background: red")
+}else {
+    divOnePM.setAttribute("style", "background: yellow")
+}
+
+// change color for 1400
+
+if( moment(loadTime).isSame(twoPM)){
+    divTwoPM.setAttribute("style", "background: green");
+} else if ( moment(loadTime).isBefore(twoPM)){
+    divTwoPM.setAttribute("style", "background: red")
+}else {
+    divTwoPM.setAttribute("style", "background: yellow")
+}
+
+//change color 1500
+
+if( moment(loadTime).isSame(threePM)){
+    divThreePM.setAttribute("style", "background: green");
+} else if ( moment(loadTime).isBefore(threePM)){
+    divThreePM.setAttribute("style", "background: red")
+}else {
+    divThreePM.setAttribute("style", "background: yellow")
+}
+
+// change color for 1600
+
+if( moment(loadTime).isSame(fourPM)){
+    divFourPM.setAttribute("style", "background: green");
+} else if ( moment(loadTime).isBefore(fourPM)){
+    divFourPM.setAttribute("style", "background: red")
+}else {
+    divFourPM.setAttribute("style", "background: yellow")
+}
+
+// change color for 1700
+
+if( moment(loadTime).isSame(fivePM)){
+    divFivePM.setAttribute("style", "background: green");
+} else if ( moment(loadTime).isBefore(fivePM)){
+    divFivePM.setAttribute("style", "background: red")
+}else {
+    divFivePM.setAttribute("style", "background: yellow")
 }
 // using moment.js find a way to to target time and change the background property with past, current and future markers
 // Past will be red, current will be green; future will be white
